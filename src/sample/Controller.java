8bd7ed0527844cb7f48a71d1b97cbc66cdb1d49e
msosic97 <msosic97@gmail.com>
2021-03-17 19:18:24 +0100

    Gotov drugi zadatak

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,11 +2,15 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
     public Label helloWorld;
+    public GridPane grid;
 
     public void btnHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("Hello World!");
+        grid.setStyle("-fx-background-color: #FFCCCB");
+
     }
 }
